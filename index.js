181814c848f1797f32034bc88ac2a1a53b326f82
shadiqur <shadiqur@netg5.com>
2017-08-23 21:52:55 -0700

    connect with mysql database

--- index.js
+++ index.js
@@ -1,37 +1,56 @@
 
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<dipendencies>>>>>>>>>>>>>>>>>>
+
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'chat'
+});
+
+connection.connect(function(err) {
+  if (err) throw err
+  console.log('You are now connected...')});
+
+
 const url = require('url'); 
 var express   =     require("express");
 var app       =     express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
-
 var session = require('express-session');
 var bodyParser = require('body-parser');
+var md5 = require('md5');
+
 
 app.use(express.static(__dirname + '/style'));
 app.use( bodyParser.json() );       
 app.use(bodyParser.urlencoded({ extended: false}));
-
 app.use(session({secret: "Shh, its a secret!",Resave: false }));
-
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 
+// <<<<<<<<<<<<<<<<<<Routs>>>>>>>>>>>>>>>
 
 
-app.get('/login',function(req,res){
-
-	console.log(req.session.name);
-	res.render('index',{name : req.session.name});
-});
 
 app.get('/',function(req,res){
 	res.sendFile(__dirname + '/home.html');
 });
 
+app.post('/register',function(req,res){
+	var registerInfo = md5(req.body.userpassword);
+	console.log(registerInfo);
+	connection.query('INSERT INTO register (name, email, password) VALUES (?, ?, ?)', [req.body.username, req.body.useremail, registerInfo], function(err, result){
+		if (err) throw err
+	});
+	console.log("you are in Register");
+
+});
 
 app.post('/',function(req,res,next){
 	
@@ -40,7 +59,7 @@ app.post('/',function(req,res,next){
 	var sessdata = req.session;
 	sessdata.name = username;
 
-	console.log(username);
+	 console.log(username);
 
   	res.redirect('/login');
 
@@ -49,10 +68,21 @@ app.post('/',function(req,res,next){
 
 
 
+
+app.get('/login',function(req,res){
+
+	// console.log(req.session.name);
+	res.render('index',{name : req.session.name});
+});
+
+
+
+
+// <<<<<<<<<<<<<<<<<Socket>>>>>>>>>>>>>>>
 io.on('connection',function(socket){
 	socket.on('mymessage',function(data){
 	 io.emit('chat message', data);
-		console.log(data.msg);
+		// console.log(data.msg);
 	});
 });
 
